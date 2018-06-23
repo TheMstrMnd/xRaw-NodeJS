f2056e2868ade203b3f5bea30d47f055826c0fa8
TheMstrMnd <themstrmnd1@gmail.com>
2018-06-24 05:31:07 +0800

    More tests

---
+++ Module/proclaim.js
@@ -0,0 +1,1 @@
+// var whisper = function (message)
---
+++ app.js
@@ -0,0 +1,7 @@
+var flight = require('./Flight');
+
+flight.setOrigin('Kensington');
+flight.setDestination('London');
+flight.setNumber(356);
+
+console.log(flight.getInfo());
